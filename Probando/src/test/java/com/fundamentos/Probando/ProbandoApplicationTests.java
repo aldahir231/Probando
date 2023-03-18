b25e7824dc5a45d9b53dcaed213263620eeba73e
aldahir231 <alramirezarredondo@gmail.com>
2023-03-18 08:13:59 -0500

    Add files via upload

---
+++ Probando/src/test/java/com/fundamentos/Probando/ProbandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.Probando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
